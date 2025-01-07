65c7b5fd3ba71c9de6f09e8c6fed37937a38549b
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-07 12:49:35 -0300

    Adicionando files

---
+++ layouts/footer.php
@@ -0,0 +1,61 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
+integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <footer>
+        <div class="rows">
+            <p>Os melhores produtos pelo melhor preço.</p>
+            <img src="./assets/imgs/logo.png" alt="Logo" class="logo">
+        </div>
+        <div class="rows">
+            <ul>
+                <li>
+                    <a href="#">Produtos</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Fale Conosco</a>
+                </li>
+            </ul>
+        </div>
+        <div class="rows">
+            <div>
+                <a href="#">
+                    <i class="fa-solid fa-phone"></i>
+                </a>
+                <p>(85)98028922</p>
+            </div>
+            <div>
+                <a href="#">
+                    <i class="fa-solid fa-envelope"></i> 
+                </a>
+                <p>comecial@gmail.com</p>
+            </div>
+            <div>
+                <a href="#">
+                    <i class="fa-solid fa-location-dot"></i>
+                </a>
+                <p>Rua: Londrina, N°2020</p>
+            </div>
+        </div>
+        <div class="rows">
+            <p>Produtos mais vendidos</p>
+            <div>
+                <img src="./assets/imgs//icons8-memoria-ram-100.png" alt="memória RAM" class="memo">
+                <img src="./assets/imgs//icons8-cpu-48.png" alt="CPU">
+                <img src="./assets/imgs//icons8-drone-48.png" alt="Drone">
+                <img src="./assets/imgs//icons8-realidade-virtual-64.png" alt="oculos realidade virtual">
+                <img src="./assets/imgs//icons8-helicóptero-100.png" alt="Helicoptero">
+            </div>
+        </div>
+    </div>
+    <div class="copy">
+        <a href="#"><i class="fab fa-facebook"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-github"></i></a>
+        <p>eCommerce @ 2022 All Right Reserved</p>
+    </div>
+</body>
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- ========== links e CDNS ============ -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+        crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+        crossorigin="anonymous"/>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <title>Document</title>
+        <link rel="stylesheet" href="./assets/css/style.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./assets/imgs/logo.png" alt="Logo" class="logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-buttons">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Fale Conosco</a>
+                        </li>
+                        <li class="nav-item">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        </li>
+                        <li class="nav-item">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
