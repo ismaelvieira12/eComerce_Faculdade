c63550c1516707dd986132c42cff2df25ae2dd31
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-01-08 17:30:29 -0300

    criando a pagina de login

---
+++ admin/header.php
@@ -0,0 +1,21 @@
+<?php session_start(); ?>
+<?php include('../server/connection.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ==================== Links para bootstrap ================ -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+    crossorigin="anonymous"></script>
+    <title>Admin</title>
+</head>
+<body>
+    
+</body>
+</html>
