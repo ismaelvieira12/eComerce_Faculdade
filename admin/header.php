d9c223f1c69fbc9b1aab051a1d5ef1e729663bb9
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-09 15:02:45 -0300

    update

--- admin/header.php
+++ admin/header.php
@@ -1,9 +1,19 @@
+<?php session_start(); ?>
+<?php include('../server/connection.php'); ?>
+
+
 <!-- // admin/header.php -->
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ======================== Links para Bootstrap ================= -->
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+    crossorigin="anonymous"></script>
     <title>Admin</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -11,5 +21,6 @@
 </head>
 <body>
     <h1 class="text-center">Admin Dashboard</h1>
+    <?php include('../admin/login.php') ?>
 </body>
 </html>
--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,26 @@
+<?php
+    if (isset($_POST["submit"])) {
+        print_r($_POST["name"]);
+        print_r($_POST["email"]);
+        print_r($_POST["password"]);
+    }
+?>
 
+
+<form action="ecommerce" method="POST">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Nome</label>
+        <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" name="password"  class="form-control" id="exampleInputPassword1">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
