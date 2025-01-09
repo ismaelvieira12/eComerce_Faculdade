798005eae99d96c0961fc6692ff93706754ee496
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-01-09 00:16:55 -0300

    Update

--- server/connection.php
+++ server/connection.php
@@ -6,4 +6,10 @@
     $usuario = "root";
     $senha = "";
 
-    
+
+// Agr eremos criar um obj para conexão
+    $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+    if ($mysqli->connect_errno) {
+        echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    };
