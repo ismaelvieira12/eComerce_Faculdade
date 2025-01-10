13941c3c6e17915a1280086c12a23aef38d76ab7
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-01-09 23:48:27 -0300

    Update

--- admin/login.php
+++ admin/login.php
@@ -14,13 +14,12 @@
     <input type="text" class="form-control" id="exampleInputTex" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <label for="exampleInputEmail1" class="form-label">Digite seu Email</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <label for="exampleInputPassword1" class="form-label">Crie uma senha</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Cadastrar</button>
 </form>
