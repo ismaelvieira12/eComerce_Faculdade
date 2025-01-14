53a406537147be55692f1c53a96ef0303c8ddd63
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-01-13 23:42:05 -0300

    Update

--- admin/login.php
+++ admin/login.php
@@ -14,7 +14,7 @@
     print_r($_POST["email"]);
     print_r($_POST["password"]);
 
-    $_sql_code = "SELECT senha, email, codigo FROM usuarios WHERE email = ' $_SESSION[email]'";
+    $_sql_code = "SELECT senha, codigo FROM usuarios WHERE email = ' $_SESSION[email]'";
     $sql_query = $mysqli->query($_sql_code) or die($mysqli->error);
     $dado = $sql_query->fetch_assoc();
     $total = $sql_query->num_rows;
