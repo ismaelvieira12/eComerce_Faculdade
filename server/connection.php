13d6daf5f12562138f79c949982518b7db82db6e
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-14 13:27:41 -0300

    Update

--- server/connection.php
+++ server/connection.php
@@ -8,10 +8,18 @@
         private $username = 'root'; // Nome de usuário
         private $password = '';  //Senha do usuário
         private $dbname = 'ecommerce'; // Nome do banco de dados criado
-        private $_conn; // Propriedade para armazenar a conexão
+        private $conn; // Propriedade para armazenar a conexão
     }
     
     // Método para criar a conexão com o banco de dados
-    function __construct() {
+    public function __construct($host, $username, $password, $dbname) {
         //Criando conexão usando OBJETO
+
+        $conn = new mysqli($host, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die('Falha na conexão'. $conn->connect_error);
+        }else{
+            echo'Conectado com sucesso!!';
+            $conn->set_charset('Conectado com Sucesso!');
+        }
     }
