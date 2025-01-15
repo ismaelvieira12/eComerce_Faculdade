56b6405d6af64c3053d8f1351b11213f7eea799f
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-15 16:28:06 -0300

    Update

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 
 
 
-<form method="post" action="">
+<!-- <form method="post" action="">
 <div>
     <label for="exampleInputEmail1" class="form-label">Digite seu Nome</label>
     <input type="text" value="" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -15,4 +15,11 @@
     <input type="password" name="senha" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" class="btn btn-primary btn">Cadastrar</button>
+</form> -->
+
+
+<form method="post" action="edit_order.php">
+  <input type="text" name="name">
+  <input type="text" name="sobrenome">
+  <button type="submit">enviar</button>
 </form>
