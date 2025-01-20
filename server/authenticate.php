27931b823c7c3d54593de09b8e3791f3ac21bd93
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-20 14:28:48 -0300

    Terminando a parte de cadastro

--- server/authenticate.php
+++ server/authenticate.php
@@ -1,28 +1,19 @@
 <!-- // server/authenticate.php -->
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>criando usuario</title>
-</head>
-<body>
-
     <?php 
         require("connection.php");
         if(isset($_POST)){
             $email = $_POST['email'];
             $nome = $_POST['name'];
             $password = $_POST['senha'];
-            $query = "INSERT INTO user (nome, email, senha) VALUES ('$nome', '$email', '$password')";
+            $query = "INSERT INTO usuarios (name, email, password) VALUES ('$nome', '$email', '$password')";
 
            //Criando regra de validação antes de enviar os dados
             $stmt = $conn->prepare($query);
             $stmt->execute();
+
+            header("Location: http://localhost/eComerce_Faculdade/");
         }
     ?>
-</body>
-</html>
 
 
--- server/connection.php
+++ server/connection.php
@@ -1,42 +1,16 @@
 <!-- Fazendo a conexão com o banco de dados -->
 
 <?php
+    $host = 'localhost';
+    $dbname = 'ecommerce';
+    $username = 'root';
+    $password = '';
 
-    // // Criando as variáveis
-    // $host = 'localhost'; // Endereço do servidor MySQL
-    // $username = 'root'; // Nome de usuário
-    // $password = '';  //Senha do usuário
-    // $dbname = 'testando'; // Nome do banco de dados criado
-    // $conn; // Propriedade para armazenar a conexão
-    
-    
-    // // Método para criar a conexão com o banco de dados
-    //     //Criando conexão usando OBJETO
-
-    //     $conn = new mysqli($host, $username, $password, $dbname);
-    //     if ($conn->connect_errno) {
-    //         die('Falha na conexão'. $conn->connect_error);
-    //     }else{
-    //         echo'Conectado com Sucesso!';
-    //         $conn->set_charset('Conectado com Sucesso!');
-    //     }
-    
-
-?>
-
-
-<?php
-$host = 'localhost';
-$dbname = 'testando';
-$username = 'root';
-$password = '';
-
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo"Conectado com sucesso!!";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
 ?>
 
