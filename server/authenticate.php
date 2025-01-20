1cb8ccc18c74e93786ae4f412074d5c17c7142a1
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-20 13:16:32 -0300

    Update

--- server/authenticate.php
+++ server/authenticate.php
@@ -8,14 +8,17 @@
     <title>criando usuario</title>
 </head>
 <body>
-    <p>enviado com sucesso</p>
 
     <?php 
-        $email = $_POST['email'];
-        $password = $_POST['password'];
         require("connection.php");
+        $email = $_POST['email'];
+        $password = $_POST['senha'];
         if(isset($_POST)){
-           $query = "INSERT INTO user (user_email, user_senha) VALUES ($email, $password)";
+           $query = "INSERT INTO user (user_email, user_senha) VALUES ('$email', '$password')";
+
+           //Criando regra de validação antes de enviar os dados
+            $stmt = $mysqli->prepare($query);
+            $stmt->excute();
         }
     ?>
 </body>
