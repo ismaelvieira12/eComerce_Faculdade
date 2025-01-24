deca5c49958dd3562ccd7e1cefa019cc46790af6
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-01-24 14:22:46 -0300

    update

--- admin/login.php
+++ admin/login.php
@@ -2,15 +2,14 @@
 <div class="login-container ">
     <h2>Login</h><br><br>
     <form method="POST" action="../server/authenticate.php" id="loginForm">
-    <div>
-        <label for="email" class="form-label">Digite seu Email</label>
-        <input type="email" value="" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Digite seu Email">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Digite sua senha</label>
-        <input type="password" name="senha" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-    </div>
-    <button type="submit" class="btn btn-primary btn">Cadastrar</button>
-</form>
+        <div>
+            <label for="email" class="form-label">Digite seu Email</label>
+            <input type="email" value="" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Digite seu Email">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Digite sua senha</label>
+            <input type="password" name="senha" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+        </div>
+        <button type="submit" class="btn btn-primary btn">Cadastrar</button>
+    </form>
 </div>
-<?php include("header.php");?>
