608ed29b68452752ae1e22a6ae23549dfa6f5259
Ismael Nascimento <117104139+ismaelvieira12@users.noreply.github.com>
2025-02-13 11:02:15 -0300

    Update connection.php

--- server/connection.php
+++ server/connection.php
@@ -10,9 +10,9 @@
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Conectado com sucesso!!";
     } catch (PDOException $e) {
         die("Connection failed: " . $e->getMessage());
     }
 ?>
+
 
