117054ed8ea6bf0cf4befcca3cd87886a428d2b2
Ismael Nascimento <117104139+ismaelvieira12@users.noreply.github.com>
2025-02-13 11:04:10 -0300

    Update authenticate.php

--- server/authenticate.php
+++ server/authenticate.php
@@ -6,7 +6,7 @@
             $email = $_POST['email'];
             $nome = $_POST['name'];
             $password = $_POST['senha'];
-            $query = "INSERT INTO usuarios (name, email, senha) VALUES ('$nome', '$email', '$password')";
+            $query = "INSERT INTO usuarios (name, email, password) VALUES ('$nome', '$email', '$password')";
 
            //Criando regra de validação antes de enviar os dados
             $stmt = $conn->prepare($query);
