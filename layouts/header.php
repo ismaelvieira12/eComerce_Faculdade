1bc0d0ec8c35559f111cb85c06628dad2d67dc8f
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-02-14 09:26:26 -0300

    update

--- layouts/header.php
+++ layouts/header.php
@@ -32,7 +32,7 @@
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Produtos</a>
+                            <a class="nav-link" href="./produtos/produtos.html">Produtos</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Blog</a>
